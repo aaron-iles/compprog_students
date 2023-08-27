e5b2f131578953848e4d2fbad52f63270715623e
Aaron Iles <aaronyostiles@gmail.com>
2023-08-27 12:03:24 -0400

    adding sublime install

---
+++ intro/install_sublime.sh
@@ -0,0 +1,13 @@
+# Update all software.
+sudo dnf update -y
+# Add the GPG keys.
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+
+# Add the repo to your repos.d folder.
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
+# Install sublime.
+sudo dnf install sublime-text
+
+# Launch sublime from the terminal.
+subl
