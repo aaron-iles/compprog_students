3c262d248552b8501fc9f5c638226e6ddeaef98a
Aaron Iles <aaronyostiles@gmail.com>
2023-08-27 12:21:54 -0400

    adding git material

---
+++ intro/clone_student_repo.sh
@@ -0,0 +1,17 @@
+# Only do this AFTER you have created your GitHub account and added an ssh key.
+
+# Enter your projects directory.
+mkdir /proj
+cd /proj
+
+# Now clone the student material repository.
+git clone git@github.com:aaron-iles/compprog_students.git
+
+# Open up the repo and take a look.
+cd compprog_students
+ls -la
+ls -la intro
+
+# Check which branch you are on.
+git status
+git branch
