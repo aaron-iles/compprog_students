e3668c836d30315fdbadc152cb9d16dd006b7827
aaron <aaronyostiles@gmail.com>
2023-09-25 18:57:50 -0400

    updating bash basics

--- intro/bash_basics.sh
+++ intro/bash_basics.sh
@@ -31,8 +31,16 @@ cd sandbox2
 # Create a blank file
 touch test_file.txt
 
-# Show the contents of the dir
+# Show the contents of the directory
 ls
+
+# Open the file with sublime. Once it is open, type "hello world", save the file, and close Sublime
+subl test_file.txt
+
+# Read the contents of the file. Does it say "hello world"?
+cat test_file.txt
+
+# Start typing "cat tes" then hit the tab button twice. Did it autocomplete to "cat test_file.txt"?
 
 # Go back one dir
 cd ..
