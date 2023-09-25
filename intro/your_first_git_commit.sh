9e49e39f37be77af2522251617b3842b20c4e614
aaron <aaronyostiles@gmail.com>
2023-09-25 18:30:38 -0400

    updating git basics

---
+++ intro/your_first_git_commit.sh
@@ -0,0 +1,34 @@
+# Install git
+sudo dnf install git -y
+
+# Change directories into your projects directory
+cd /proj/
+
+# Clone your own git repo
+git clone git@github.com:<your GitHub username>/compprog_class.git
+
+# Enter your git repo directory
+cd /proj/compprog_class/
+
+# Open a file in this current directory with Sublime and write something in it
+subl test.txt
+
+# Now stage the file
+git add test.txt
+
+# Commit the file 
+git commit -m "adding a test file for my class assignment"
+
+# Push the commit
+git push
+
+
+
+# That's it! Now here is some more information...
+
+# If you made changes to your git repo elsewhere and needed to pull them down to your VM
+cd /proj/compprog_class/
+git pull
+
+# Now say you made changes to a file that you wanted to undo
+git checkout -- some_file_i_wanted_to_undo.txt
