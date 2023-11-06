8c140a3d6aac18bd603c7248e75bd361620da5d9
aaron <aaronyostiles@gmail.com>
2023-11-06 18:28:16 -0500

    adding variable assignment

---
+++ basics/variable_assignment.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+# These are permitted assignments:
+a = 5
+b = True
+c1 = 'my string'
+my_var = [1, 2, 3]
+_private = 2.2
+_ = {"a": "b"}
+Def = "my other string"
+
+
+# These are illegal assignments:
+# Variables may not start with numbers.
+1x = 99
+# Variables may not use reserved keywords.
+def = 'something'
+pass = [1, 2, 3]
+return = False
+# Variables may not contain spaces.
+my var = "hello"
