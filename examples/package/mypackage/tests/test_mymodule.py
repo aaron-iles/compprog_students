14e39d335c03f2a663ed3f166ef7cb7d370c5217
Aaron Iles <aaronyostiles@gmail.com>
2024-01-30 14:39:39 -0500

    renaming

---
+++ examples/package/mypackage/tests/test_mymodule.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# You're allowed to do either of these!
+from mypackage import mymodule
+import mypackage
+
+
+def test_add_1_and_2():
+    assert mypackage.mymodule.add(1, 2) == 3
+
+def test_add_2_and_3():
+    assert mymodule.add(2, 3) == 5
+
