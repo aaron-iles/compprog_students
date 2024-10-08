1a05043c4a4bafe8bc4098574e513d017a0af555
aaron <aaronyostiles@gmail.com>
2024-10-08 08:39:50 -0400

    adds lambda functions

---
+++ module_5_more_advanced_topics/lambda_functions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# 01: Write a filter with a lambda function that will filter out all values from the list that are 
+# greater than 5.
+
+# 02: Write a map with a lambda function that will square every value in a list.
+
+# 03: Write a map with a lambda function that will take every item in a list and append the string 
+# ".com" to it.
+
+# 04: Write a filter with a lambda function that will filter out all values from a list whose 
+# length is greater than 1.
+
+# 05: Write a filter with a lambda function that will remove all items from the list below that do 
+# not contain the "name" key.
+list_to_filter = [
+    {
+        "name": "Isaiah",
+        "enrolled": True
+    },
+    {
+        "name": "Shaedon",
+        "assigned": False
+    },
+    {
+        "is_cat": True,
+        "pet_name": "Rudy"
+    },
+    {
+        "id": "Jordan",
+        "number": 19
+    }
+]
